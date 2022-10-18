5daabd00af7ac3e105a102e14efb9876e0fcef4f
Arya Keni <47888500+codersupreme99101@users.noreply.github.com>
2022-10-18 18:43:15 -0400

    v1

---
+++ final_project/data_down.sh
@@ -0,0 +1,6 @@
+export KAGGLE_USERNAME=aryakeni1
+export KAGGLE_KEY=7177a6a52c7609d96cbf1f1a76e24d0f
+
+#kaggle kernels output jedrzejdudzicz/pneumonia-detection-on-chest-x-ray-accuracy-92 -p inputs
+
+kaggle datasets download jedrzejdudzicz/pneumonia-detection-on-chest-x-ray-accuracy-92/data -p inputs
